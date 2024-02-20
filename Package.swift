bec86eba1fec4e51cd11a0e80a976e14627df793
yanun0323 <a1256212562@gmail.com>
2024-02-20 10:55:37 +0800

    update: swift tool to 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -30,6 +30,12 @@ let package = Package(
                 .product(name: "SQLite", package: "sqlite.swift")
             ]
         ),
+//        .executableTarget(
+//            name: "Sworm",
+//            dependencies: [
+//                .product(name: "SQLite", package: "SQLite.swift")
+//            ]
+//        ),
         .testTarget(
             name: "SwormTests",
             dependencies: ["Sworm"]
